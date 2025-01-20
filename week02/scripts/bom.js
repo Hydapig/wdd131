c6d796d0c7e9d959baf1f75098e22d79c7c87181
hydapig <moh8287@gmail.com>
2025-01-20 16:23:38 -0700

    updated bom js

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,11 +1,25 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('list');
+const list = document.querySelector('ul');
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+
+button.addEventListener('click', function() {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    if (input.value.trim() !=='') {
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+    }
+
+    deleteButton.addEventListener('click', function() {
+        list.removeChild(li);
+        input.focus();
+    });
+
+    input.value='';
+    input.focus();
+});
