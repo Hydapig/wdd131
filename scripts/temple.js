e0346068d2f3392c8b25726808369d29ea5aa542
hydapig <moh8287@gmail.com>
2025-01-21 00:37:18 -0700

    completed temple album

---
+++ scripts/temple.js
@@ -0,0 +1,13 @@
+const date = new Date();
+let year = date.getFullYear();
+const formattedDate = date.toLocaleString('en-US', {timeZoneName: 'short'});
+document.getElementById('year').innerHTML = year
+document.getElementById('lastModified').innerHTML = formattedDate
+
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
