cee1ffa106ac80006f61893d578677bf3b15096b
hydapig <moh8287@gmail.com>
2025-01-25 21:06:39 -0700

    testing importing js variables

--- scripts/place.js
+++ scripts/place.js
@@ -3,3 +3,6 @@ let year = date.getFullYear();
 const formattedDate = date.toLocaleString('en-US', {timeZoneName: 'short'});
 document.getElementById('year').innerHTML = year
 document.getElementById('lastModified').innerHTML = formattedDate
+
+import {myVariable} from './variables.js';
+console.log(myVariable);
